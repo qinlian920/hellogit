e8b03331e62a84aed78f721c1a1abb337d77c73b
47163 <471634910@qq.com>
2021-01-11 17:01:27 +0800

    创建分支

--- src/main/java/com/self/git/HelloGit.java
+++ src/main/java/com/self/git/HelloGit.java
@@ -8,5 +8,6 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello GIT!");
         System.out.println("update1");
+        System.out.println("创建分支");
     }
 }
