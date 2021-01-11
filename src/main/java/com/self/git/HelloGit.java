c8877ddbb151642b6dc43e8b5bf9fd7da2246f59
47163 <471634910@qq.com>
2021-01-11 16:46:49 +0800

    it is my first git

---
+++ src/main/java/com/self/git/HelloGit.java
@@ -0,0 +1,11 @@
+package com.self.git;
+
+/**
+ * @author 47163
+ * @date 2021-01-11 15:25
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello GIT!");
+    }
+}
